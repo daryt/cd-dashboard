2b50ce4218d1946c95dcc05796b60ef92b8d39f8
daryt <daryt@users.noreply.github.com>
2015-03-23 15:52:53 -0700

    added notes page

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,6 +4,8 @@ $route['default_controller'] = "mains";
 $route['404_override'] = '';
 $route['admin'] = "admins";
 $route['dashboard'] = "users";
+$route['notes'] = "mains/notes";
+
 
 
 //end of routes.php
